b649e25f624d61437998805aab23b5334ced8c72
Zhi Huang <huangzz@umich.edu>
2015-10-30 12:47:47 -0400

    Shalabh work
    I fixed a bug

--- prescan pedestrian info.java
+++ prescan pedestrian info.java
@@ -1,10 +1,1 @@
-//292 Nim Game
-
-public class Solution {
-    public boolean canWinNim(int n) {
-        if(n%4 != 0){
-            return true;
-        }
-        return false;
-    }
-}
+ped1 = 1;
