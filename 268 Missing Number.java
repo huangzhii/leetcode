2c01b0f38187bd6f8d074207cf93a2a93e903725
Zhi Huang <huangzz@umich.edu>
2015-11-17 19:52:20 -0500

    bit manipulation

---
+++ 268 Missing Number.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++){
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
